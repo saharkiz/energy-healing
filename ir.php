ecbb12db0eb29840c0d69f73442a2527b7b012bc
Aresh Saharkhiz <saharkiz@yahoo.com>
2019-11-12 19:23:09 +0400

    Update ir.php

--- ir.php
+++ ir.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Energy Healing | Herbal & Traditional Medicine Dubai</title>
+    <title>شفا انرژی | گیاهان دارویی و سنتی دبی</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="geo.placename" content="Dubai" />
@@ -57,19 +57,19 @@
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light scroller scrolled" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="/"><span><img src="images/energy-healing-dubai_logo.png" style="width:20%" class="img-responsive" alt="Energy healing, Reiki, Herbal Medicine, Dubai" /> &nbsp; Energy Healing</span></a>
+	      <a class="navbar-brand" href="/"><span><img src="images/energy-healing-dubai_logo.png" style="width:20%" class="img-responsive" alt="Energy healing, Reiki, Herbal Medicine, Dubai" /> &nbsp; انرژی درمانی</span></a>
 	      <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav nav ml-auto">
-	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="home"><span>Home</span></a></li>
-	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="about"><span>About</span></a></li>
-	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="Services"><span>Services</span></a></li>
-	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="testimony"><span>Testimony</span></a></li>
-	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="contact"><span>Contact</span></a></li>
-	          <li class="nav-item cta"><a href="https://wa.me/971506271565?text=Energy%20Healing%20Website%20Inquiry" class="nav-link">Call Us</a></li>
+	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="home"><span>صفحه اول</span></a></li>
+	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="about"><span>درباره ما</span></a></li>
+	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="Services"><span>خدمات</span></a></li>
+	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="testimony"><span>شهادت</span></a></li>
+	          <li class="nav-item"><a href="#" class="nav-link" data-nav-section="contact"><span>تماس با ما</span></a></li>
+	          <li class="nav-item cta"><a href="https://wa.me/971506271565?text=Energy%20Healing%20Website%20Inquiry" class="nav-link">با ما تماس بگیرید</a></li>
 			<li class="nav-item"><a href="/ae.php" class="nav-link"><img src="images/ae.png" alt="Arabic Language" class="rounded-circle"></a></li>
 			<li class="nav-item"><a href="/ir.php" class="nav-link"><img src="images/ir.png" alt="Farsi Language" class="rounded-circle"></a></li>
 			<li class="nav-item"><a href="/" class="nav-link"><img src="images/us.png"  alt="English Language" class="rounded-circle"></a></li>
@@ -86,11 +86,11 @@
           	<p class="d-flex align-items-center" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">
 							<a href="https://www.youtube.com/watch?v=TvYqgDDl0-k" class="icon-video popup-vimeo d-flex justify-content-center align-items-center mr-3">
     						<span class="ion-ios-play play mr-2"></span>
-    						<span class="watch">Watch Video</span>
+    						<span class="watch">تماشای فیلم</span>
     					</a>
 						</p>
-            <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Energy Therapy to Accelerate Healing </h1>
-            <p class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Simple. Effective Techniques to Experience Joy!</p>
+            <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">انرژی درمانی برای تسریع بهبودی </h1>
+            <p class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">ساده. تکنیک های موثر برای تجربه شادی!</p>
           </div>
         </div>
       </div>
@@ -103,9 +103,9 @@
             <div class="media block-6 services text-center d-block">
               <div class="icon"><span class="flaticon-layers"></span></div>
               <div class="media-body">
-                <h3 class="heading mb-3">Healing in Person</h3>
-                <p>A face to face session, recommended to do once a week to allow time for the frequency to manifest fully.</p>
-                <p><a href="#" class="btn btn-primary">Read more</a></p>
+                <h3 class="heading mb-3">جلسه خصوصی</h3>
+                <p>یک جلسه چهره به چهره ، توصیه می شود هفته ای یک بار انجام دهید تا زمان لازم برای تظاهرات کامل صورت گیرد.</p>
+                <p><a href="#" class="btn btn-primary">بیشتر بخوانید</a></p>
               </div>
             </div>
           </div>
@@ -113,9 +113,9 @@
             <div class="media block-6 services text-center d-block">
               <div class="icon"><span class="flaticon-compass-symbol"></span></div>
               <div class="media-body">
-                <h3 class="heading mb-3">Distance Healing</h3>
-                <p>In case a face to face session is not possible, we offer Distance Healing Session which many people feel could be much stronger.</p>
-                <p><a href="#" class="btn btn-primary">Read more</a></p>
+                <h3 class="heading mb-3">شفا از راه دور</h3>
+                <p>در صورت عدم امکان جلسات چهره به چهره ، ما جلسه شفا از راه دور را ارائه می دهیم که بسیاری از افراد احساس می کنند می توانند بسیار قوی تر باشند.</p>
+                <p><a href="#" class="btn btn-primary">بیشتر بخوانید</a></p>
               </div>
             </div>
           </div>
@@ -123,9 +123,9 @@
             <div class="media block-6 services text-center d-block">
               <div class="icon"><span class="flaticon-layers"></span></div>
               <div class="media-body">
-                <h3 class="heading mb-3">Consultation Healing</h3>
-                <p>A face to face session that will last about 45-60 minutes on Traditional medicine and Herbal treatments</p>
-                <p><a href="#" class="btn btn-primary">Read more</a></p>
+                <h3 class="heading mb-3">مشاوره</h3>
+                <p>جلسه رو در رو که حدود 45 تا 60 دقیقه در طب سنتی و معالجات گیاهی به طول خواهد انجامید</p>
+                <p><a href="#" class="btn btn-primary">بیشتر بخوانید</a></p>
               </div>
             </div>
           </div>
@@ -142,11 +142,11 @@
           <div class="col-md-6 col-lg-8 pl-lg-5 py-5">
             <div class="row justify-content-start pb-3">
               <div class="col-md-12 heading-section ftco-animate">
-                <span class="subheading">Welcome</span>
-                <h2 class="mb-4">About our Healer</h2>
-                <p>Energy healing is known for a very long time all over the world and since it works succesfully on body and mind in a holistic way, it is a lot to process.</p>
-                <p>Eng. Asghar Saharkhiz started Reiki Energy Therapy in 2006. In 2008 he started Magnetic Therapy. Healing many patients thru Magentic Therapy and Energy Healing. </p>
-                <p>Author of 2 Books "Miracle Cure with Magnet" and "Miracle Cure with Nature"</p>
+                <span class="subheading">خوش آمدید</span>
+                <h2 class="mb-4">درباره شفا دهنده ما</h2>
+                <p>بهبود انرژی برای مدت زمان طولانی در سراسر جهان شناخته شده است و از آنجا که با موفقیت روی بدن و ذهن به صورت جامع عمل می کند ، پردازش آن بسیار زیاد است.</p>
+                <p>مهندس انرژی درمانی ریکی را از سال 2006 شروع کرد. در سال 2008 مغناطیس درمانی را آغاز کرد. بسیاری از بیماران از طریق داروهای درمانی و التیام بخش انرژی شفا می یابند. </p>
+                <p>نویسنده 2 کتاب</p>
               </div>
             </div>
             <div class="row">

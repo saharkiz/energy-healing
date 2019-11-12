af3a2bfba0f700d074ddbabea706db17599e754e
Aresh Saharkhiz <saharkiz@yahoo.com>
2019-11-12 09:59:36 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
     <link rel="stylesheet" href="css/aos.css">
 
     <link rel="stylesheet" href="css/ionicons.min.css">

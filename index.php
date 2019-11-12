d1d39ddb164b7b10db6f0f14f110fa32c82bd464
Aresh Saharkhiz <saharkiz@yahoo.com>
2019-11-12 10:18:36 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -272,7 +272,7 @@
     <div id="instagram-feed1" class="instagram_feed">Loading....</div>
       </div>
    </div>
-
+  </section>
 
     <footer class="ftco-footer ftco-section">
       <div class="container">
@@ -323,10 +323,9 @@
         </div>
         <div class="row">
           <div class="col-md-12 text-center">
-
             <p>
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="icon-heart color-danger" aria-hidden="true"></i>
-</p>
+              Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="icon-heart color-danger" aria-hidden="true"></i>
+            </p>
           </div>
         </div>
       </div>
